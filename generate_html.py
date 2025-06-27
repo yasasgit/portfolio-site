8089fcc863b23839231617d63fa60c12b964a225
Yasas Harshana <72686358+yasasgit@users.noreply.github.com>
2025-06-28 03:54:17 +0530

    init

---
+++ generate_html.py
@@ -0,0 +1,16 @@
+html_content = """
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Yasas Harshana - Portfolio</title>
+</head>
+<body>
+    <h1>Hello, I'm Yasas Harshana</h1>
+    <p>This is my simple portfolio page.</p>
+</body>
+</html>
+"""
+
+with open("index.html", "w") as f:
+    f.write(html_content)
